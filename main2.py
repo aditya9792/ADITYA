8d4b2cd0a69718e1f5e3b4b7143771d0688f7d32
YEKBOTE NAG ADITYA KUMAR <ynaditya2003@gmail.com>
2025-06-23 20:48:37 +0530

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,8 @@
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Concatenating first and last name
+full_name = first_name + " " + last_name
+
+# Displaying personalized greeting
+print(f"\nHello, {full_name}! Welcome to the program.")
